6ce969d5c1ca57a0e5425f7b2a53c6b14618e064
chandrasekhar009 <31537403+chandrasekhar009@users.noreply.github.com>
2017-10-11 15:39:25 +0530

    commit project7

---
+++ src/project6/project7.java
@@ -0,0 +1,18 @@
+package project6;
+
+import java.util.Scanner;
+
+public class project7 {
+
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter username");
+		String u=sc.nextLine();
+		System.out.println("enter password");
+		String p=sc.nextLine();
+		
+
+	}
+
+}
